c8000609791b02ffb44fb2ec68bf3bb6330c0e26
edgarxene <edgarxene@gmail.com>
2018-04-05 18:49:51 -0500

    add

--- Hola Mundo/1. Hola Mundo/src/app/app.component.ts
+++ Hola Mundo/1. Hola Mundo/src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  nombre:string = "Edgar";
-  apellido:string = "Cruz";
+  
 }
