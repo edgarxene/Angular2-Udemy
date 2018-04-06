bb5e66c5a89d2b5232386875d6e8610f838d0f24
Edgar Cruz <edgarxene@gmail.com>
2018-04-05 23:30:05 -0500

    body hola mundo

--- Hola Mundo/1. Hola Mundo/src/app/components/body/body.component.ts
+++ Hola Mundo/1. Hola Mundo/src/app/components/body/body.component.ts
@@ -8,4 +8,12 @@ export class BodyComponent {
   title = 'app';
   nombre:string = "Edgar";
   apellido:string = "Cruz";
+  frase:any ={
+    mensaje : "Un gran poder, requiere una gran responsabilidad",
+    autor : "Ben Parker"
+  }
+  mostrar:boolean = false;
+
+  personajes:string[] = ["Spiderman", "Venom", "Octopus"];
+
 }
