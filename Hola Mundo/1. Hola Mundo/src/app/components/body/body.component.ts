f772064d8e322f2196814701af561e3cce6ebce3
Edgar Cruz <edgarxene@gmail.com>
2018-04-05 21:54:24 -0500

    ok

--- Hola Mundo/1. Hola Mundo/src/app/components/body/body.component.ts
+++ Hola Mundo/1. Hola Mundo/src/app/components/body/body.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  templateUrl: './body.component.html'
 })
 export class BodyComponent {
   title = 'app';
