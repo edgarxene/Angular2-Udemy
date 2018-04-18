e672f12054dca7eceb5a40feb5d6652703016031
edgarxene <edgarxene@gmail.com>
2018-04-17 23:35:35 -0500

    pipes

--- 4. Pipes/src/app/app.component.ts
+++ 4. Pipes/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Edgar';
+  nombre2 = 'eDGar saMUel cRuz albarran';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1350.50;
+
+  heroe = {
+    nombre : "Logan",
+    clave : "Wolvrine",
+    edad : "",
+    direccion: {
+      calle: "primera",
+      numero: 19
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("LLego la data!!!"),3500);
+  })
+
+  fecha = new Date();
+
 }
--- 4. Pipes/src/app/app.module.ts
+++ 4. Pipes/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {CapitalizadoPipe} from './pipes/capitalizado.pipe';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ imports: [ BrowserModule ],
+ declarations: [ AppComponent, CapitalizadoPipe ],
+ providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
+ bootstrap: [ AppComponent ]
 })
 export class AppModule { }
